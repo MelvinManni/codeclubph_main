c2c094571d2a33349eb6bb2679d49aa25a48928e
MelvinManni <melvinnnamani@gmail.com>
2020-03-16 12:20:33 +0100

    Desktop look correction

---
+++ src/navBar.js
@@ -0,0 +1,15 @@
+let navBar = document.getElementById("navBar");
+
+
+
+window.addEventListener("scroll", ()=>{
+    let Offset = pageYOffset;
+    
+    if (Offset > 120){
+        navBar.style.backgroundColor = "rgba(255, 255, 255, 0.8)"
+    } else{
+        navBar.style.backgroundColor = "rgba(255, 255, 255, 1)"   
+    }
+});
+
+
