4c05bd54505b5c10cd694bb07900d4cf8a8f1058
MelvinManni <melvinnnamani@gmail.com>
2020-03-21 17:21:28 +0100

    E dinnor work well Ooo

--- src/navToggle.js
+++ src/navToggle.js
@@ -1,22 +1,8 @@
-
-let navIcon = document.getElementById("navIcon");
-let navImg = document.getElementById("navImg");
-
-
-
-
-
-
-// navIcon.addEventListener("click", ()=>{
-
-    navImg.addEventListener("click", ()=>{
-
-        if (navImg.src = "assets/images/icon/close.svg") {
-            navImg.src = "assets/images/icon/menu.svg"
-        }
-        //  else {
-        //     navImg.src = "assets/images/icon/close.svg";
-        // } 
+$(function () {
+    $('#toggleNav').on('hide.bs.collapse', function () {
+        $('.navbar-toggler').removeClass('open');
     })
-
-// })
+    $('#toggleNav').on('show.bs.collapse', function () {
+        $('.navbar-toggler').addClass('open');
+    })
+});
